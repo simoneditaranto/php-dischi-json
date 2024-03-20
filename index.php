059eea896dba1f16a3446908076095d3ec4f8f4a
s.ditaranto <simodita99@gmail.com>
2024-03-20 15:04:55 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dischi Json</title>
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- style -->
+    <link rel="stylesheet" href="./css/style.css">
+
+</head>
+
+<body>
+
+    <header>
+        
+        <div class="container">
+            
+            <img src="https://th.bing.com/th/id/R.c2a12cba057711495849088844f950cc?rik=982VbYW4HE09lQ&riu=http%3a%2f%2f1000logos.net%2fwp-content%2fuploads%2f2017%2f08%2fSpotify-Logo.png&ehk=Gcl8lyv0FdI4gDVyKLmVCVfNf8yc1xDmAvEDyPzt8Bk%3d&risl=&pid=ImgRaw&r=0" alt="logo spotify" class="logo">
+        
+    
+        </div>
+
+    </header>
+
+    <main class="container mt-3 px-5 pb-5">
+
+        <div class="cards row justify-content-between gap-3">
+            <div class="card col-3 d-flex flex-column align-items-center gap-2 py-4 text-center text-white">
+                <div class="poster">
+                    <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="" class="poster-image">
+                </div>
+                <div class="title fw-bold">
+                    New Jersey
+                </div>
+                <div class="author">
+                    Bon Jovi
+                </div>
+                <div class="year fw-bold">
+                    1988
+                </div>
+            </div>
+            <div class="col-3 text-danger">testo</div>
+            <div class="col-3 text-danger">testo</div>
+            <div class="col-3 text-danger">testo</div>
+            <div class="col-3 text-danger">testo</div>
+            <div class="col-3 text-danger">testo</div>
+        </div>
+
+    </main>
+        
+
+    <!-- bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+</body>
+
+</html>
